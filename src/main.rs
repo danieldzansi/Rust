bcaaabfa42f7d5e2ff3aa715515fb30c82da5227
danieldzansi <danieldzansi96@gmail.com>
2026-02-18 12:10:46 +0000

    more

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 mod variable;
+mod operators ;
 // mod variables;
 fn main() {
     println!("Hello, world!");
     variable::run();
     variable::runn();
+    operators::operator();
 }
---
+++ src/operators.rs
@@ -0,0 +1,12 @@
+pub fn operator (){
+    let add = 5+3;
+    let sub = 8-5;
+    let div = 60/3;
+    let mul = 5*50;
+
+     println!("Add : {}",add);
+     println!("sub : {}",sub);
+     println!("div : {}",div);
+     println!("mul : {}",mul);
+
+}
