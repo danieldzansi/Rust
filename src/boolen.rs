f766e6804fcb926521980dbdbbe6ce19bd92d345
danieldzansi <danieldzansi96@gmail.com>
2026-02-18 12:24:14 +0000

    bool

---
+++ src/boolen.rs
@@ -0,0 +1,16 @@
+pub fn bool (){
+    let is_programming_fun : bool =  true ;
+    let is_rust_a_language : bool =  false ;
+    let is_log_in :bool =true ;
+
+      println!("is programming fun : {}" , is_programming_fun);
+      println!("is rust a language : {}" ,is_rust_a_language);
+
+    if is_log_in  {
+        println!("welcome");
+    }else {
+        println!("please log in to continue");
+    }
+
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 mod variable;
 mod operators ;
+mod boolen ;
 // mod variables;
 fn main() {
     println!("Hello, world!");
     variable::run();
     variable::runn();
     operators::operator();
+    boolen :: bool();
 }
